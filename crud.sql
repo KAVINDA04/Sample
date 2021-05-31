d48f289b6e633a4e9868c8c6c0d29d55a687148f
KAVINDA04 <akilakavinda04@gmail.com>
2021-05-31 18:16:25 +0530

    Update_01

--- crud.sql
+++ crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2021 at 12:54 PM
+-- Generation Time: May 31, 2021 at 02:45 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.29
 
@@ -53,8 +53,8 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2019_08_19_000000_create_failed_jobs_table', 1),
-(2, '2021_05_29_062114_create_users_table', 1);
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2021_05_29_062114_create_users_table', 1);
 
 -- --------------------------------------------------------
 
@@ -64,11 +64,22 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstName`, `lastName`, `mobile`, `email`, `created_at`, `updated_at`) VALUES
+(1, 'akila', 'kavinda', '0774467943', 'akilakavinda04@gmail.com', '2021-05-31 03:42:14', '2021-05-31 03:42:14'),
+(2, 'sasika', 'devinda', '0770280908', 'devinda@gmail.com', '2021-05-31 03:42:47', '2021-05-31 03:42:47'),
+(3, 'Roy', 'Pearson', '0774467943', 'akilakavinda04@gmail.com', '2021-05-31 06:34:23', '2021-05-31 06:34:23');
 
 --
 -- Indexes for dumped tables
@@ -106,13 +117,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
